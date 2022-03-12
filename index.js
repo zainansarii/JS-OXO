d02c4761bc3e52b7e669f29f388747c52f81f87a
Zain Ansari <zain@Zains-MacBook-Air.local>
2022-03-12 14:15:34 +0000

    added reset button and game logic

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 window.addEventListener('DOMContentLoaded', () => {
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const playerDisplay = document.querySelector('.display-player');
-    const resetButton = document.querySelector('#reset');
+    const resetButton = document.querySelector('.reset');
     const announcer = document.querySelector('.announcer');
 
     // this is just the in-memory board state!
-    let board = ['','','','','','','','','']
+    let board = ['','','','','','','','',''];
+    let currentPlayer = 'X'
 
-    const updateBoard = (tile, index) => {
-        tile.innerText = "X"
+    const tileIsOccupied = (index) => {
+        if(board[index]!=""){
+            return true
+        }
+        return false 
     }
 
+    const updateBoard = (tile, index) => {
+
+        // maybe just update the board in memory then send that to the front end? 
+        // as opposed to updating both seperately
+
+        if(!tileIsOccupied(index)){
+            tile.innerText = currentPlayer
+            board[index] = currentPlayer
+            if(currentPlayer=='X'){
+                currentPlayer='O'
+            }
+            else{
+                currentPlayer='X'
+            }
+        }
+        console.log(board)
+    }
+
+    const 
+
     tiles.forEach((tile, index) => {
-        console.log(tile)
         tile.addEventListener('click', () => updateBoard(tile, index));
     })
 
+    resetButton.addEventListener('click', () => resetBoard())
 
 })
