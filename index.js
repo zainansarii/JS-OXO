3a22b01f612addf658b4bdea5ec89d71b84a8171
Zain <zain.ansari@live.com>
2022-03-12 19:45:57 +0000

    completed base tic-tac-toe game

--- index.js
+++ index.js
@@ -6,39 +6,107 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // this is just the in-memory board state!
     let board = ['','','','','','','','',''];
-    let currentPlayer = 'X'
+    let currentPlayer = 'X';
+    let gameIsActive = true;
+
 
     const tileIsOccupied = (index) => {
         if(board[index]!=""){
-            return true
+            return true;
         }
-        return false 
+        return false ;
+    }
+
+    const switchPlayer = () => {
+        if(currentPlayer=='X'){
+            currentPlayer='O';
+        }
+        else{
+            currentPlayer='X';
+        }
+    }
+
+    const horizontalWin = () => {
+        for(let i = 0; i < 9; i+=3){
+            if(board[i] != ""){
+                if(board[i] == board[i+1] && board[i+1] == board[i+2]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    const verticalWin = () => {
+        for(let i = 0; i < 3; i++){
+            if(board[i] != ""){
+                if(board[i] == board[i+3] && board[i+3] == board[i+6]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    const diagonalWin = () => {
+        if(board[0] != ""){
+            if(board[0] == board[4] && board[4] == board[8]){
+                return true;
+            }
+        }
+        if(board[2] != ""){
+            if(board[2] == board[4] && board[4] == board[6]){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    const checkWin = () => {
+        if(horizontalWin() || verticalWin() || diagonalWin()){
+            return true;
+        }
+        return false;
+    }
+
+    const setWinner = () => {
+        announcer.innerHTML = 'Player ' + currentPlayer + ' wins! <br><br>'
+        announcer.classList.remove('hide')
     }
 
     const updateBoard = (tile, index) => {
-
-        // maybe just update the board in memory then send that to the front end? 
-        // as opposed to updating both seperately
-
-        if(!tileIsOccupied(index)){
-            tile.innerText = currentPlayer
-            board[index] = currentPlayer
-            if(currentPlayer=='X'){
-                currentPlayer='O'
-            }
-            else{
-                currentPlayer='X'
+        if(gameIsActive){
+            if(!tileIsOccupied(index)){
+                tile.innerText = currentPlayer;
+                board[index] = currentPlayer;
+                if(checkWin()){
+                    setWinner()
+                    //freeze game
+                }
+                else{
+                    switchPlayer();
+                }
             }
         }
-        console.log(board)
     }
 
-    const 
+    const resetBoard = () => {
+        tiles.forEach((tile, index) => {
+            tile.innerText='';
+            board[index]='';
+        })
+    }
 
+    const resetGame = () => {
+        resetBoard();
+        currentPlayer='X'
+        announcer.classList.add('hide')
+    }
+
+    
     tiles.forEach((tile, index) => {
         tile.addEventListener('click', () => updateBoard(tile, index));
     })
 
-    resetButton.addEventListener('click', () => resetBoard())
-
+    resetButton.addEventListener('click', () => resetGame());
 })
